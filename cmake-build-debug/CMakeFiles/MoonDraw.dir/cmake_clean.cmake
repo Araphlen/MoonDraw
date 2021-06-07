6d7bc3d2fbe3217b06400ff7cd80c253c98f14e5
Araphlen <pro.martin.mickael@gmail.com>
2021-06-07 16:30:58 +0200

    fichier du prof

---
+++ cmake-build-debug/CMakeFiles/MoonDraw.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MoonDraw.dir/main.cpp.o"
+  "MoonDraw"
+  "MoonDraw.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MoonDraw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
